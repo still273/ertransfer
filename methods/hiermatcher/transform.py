e8586d03cfe0977f8f959982a04128de2c50c96c
Oleh Astappiev <astappiev@l3s.de>
2025-01-29 20:02:55 +0100

    feat(hiermatcher): freeze dependencies + save train/eval time

--- methods/hiermatcher/transform.py
+++ methods/hiermatcher/transform.py
@@ -2,13 +2,13 @@ import os
 import pandas as pd
 
 
-def transform_output(stats, runtime, max_mem, dest_dir):
-    # save evaluation metrics to metrics.csv
+def transform_output(stats, train_time, eval_time, dest_dir):
     pd.DataFrame({
         'f1': [stats.f1().item()],
         'precision': [stats.precision().item()],
         'recall': [stats.recall().item()],
-        'max_mem': [max_mem],
-        'time': [runtime],
+        'train_time': [train_time],
+        'eval_time': [eval_time],
     }).to_csv(os.path.join(dest_dir, 'metrics.csv'), index=False)
+
     return None
